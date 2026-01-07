8184947654027e4df53471ffab5942faeb1d2268
Priyanshu Sharma <legendps9395@gmail.com>
2026-01-07 13:05:44 +0530

    limited tries for number guessing
    Added a limited number of tries for guessing the number. i also learnt f strings and try and except

--- Number guess.py
+++ Number guess.py
@@ -1,7 +1,8 @@
 import random
 guess_no = random.randint(1, 100)
-print(guess_no + random.randint(1, 100))
-while True :
+tries = 6
+print(f"you only have {tries} tries to guess")
+for x in range(tries):
     try:
         guess = int(input("Guess a no between 1 and 100\n"))
         if guess < guess_no :
@@ -12,5 +13,6 @@ while True :
             print("Congratulaions! you guessed the number")
             break
     except ValueError:
-        print("Please enter a valid no")
-
+            print("Please enter a valid no")
+else:
+    print("you lost to guess")
