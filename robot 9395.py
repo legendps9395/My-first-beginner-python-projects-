d20b89f42930231daefcde97a840087b26a619cd
Priyanshu Sharma <legendps9395@gmail.com>
2026-01-07 12:36:35 +0530

    Bob's evil status check and improve input handling
    i just fixed some minor errors and other errors like ValueError and nameError, I don't know how to fix

--- robot 9395.py
+++ robot 9395.py
@@ -1,18 +1,26 @@
 print("Hello welcome to priyanshu coffee shop ")
 name = input("what is your name?\n" ).title()
-if name == "Bob":
-    evil_status = input("Are you evil?\n").lower
-    good_deeds = int(input("How many good deeds have you done today?\n"))
-    if evil_status == "yes" and good_deeds <4 :
-        print("You are not welcome here, get out from here")
-        exit()
-    if evil_status == "no":
-     print("Oh! so you are one of those good Bobs. You are welcome here")
-else : print("Hello " + name + " thank you so much for coming in today!!")
-menu = "Black Coffee :- $3,\n Espresso :- 4$,\n Latte :- $6,\n Cappucino :- $6.5,\n Frappuchino :- $7.5,\n Classical Coffee :- $2"
 
+if name == "Bob":
+     while True:
+        evil_status = input("Are you evil?\n").lower()
+        if evil_status == "yes":
+            good_deeds = int(input("How many good deeds have you done today?\n"))
+            if evil_status == "yes" and good_deeds <4 :
+                print("You are not welcome here, get out from here")
+                time.sleep(3)
+                exit()
+        if evil_status == "no" or good_deeds >=4 :
+            print("Oh! so you are one of those good Bobs. You are welcome here")
+            break
+        else:
+            print("Give a valid answer.")
+else : print("Hello " + name + " thank you so much for coming in today!!")
+    
+menu = "Black Coffee :- $3,\n Espresso :- 4$,\n Latte :- $6,\n Cappucino :- $6.5,\n Frappuchino :- $7.5,\n Classical Coffee :- $2"
 print(name + ", What would you like from our menu today? "
 "Here is what we are serving today.\n" + menu  )
+
 while True:
     order = input().lower()
     if order == "black coffee":
@@ -40,4 +48,5 @@ quantity = input("How many " + order.title() + " would you like?\n")
 total = price * int(quantity)
 print ("Your total is: $" + str(total))
 print("Sounds good, we will have your " + quantity +" "+ order + " ready in a moment") 
+
 
